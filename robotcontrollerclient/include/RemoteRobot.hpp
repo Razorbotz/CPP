1009b1226ab2db1c082168cb4fc6a59b086d1da3
Luke Simmons <luke5083@live.com>
2022-02-26 11:38:19 -0600

    Add documentation for RemoteRobot
    For include/RemoteRobot.hpp and src/RemoteRobot.cpp.

--- robotcontrollerclient/include/RemoteRobot.hpp
+++ robotcontrollerclient/include/RemoteRobot.hpp
@@ -1,7 +1,15 @@
-//
-// Created by luke on 2/11/22.
-//
-
+/** @file
+* @brief Declares a struct, RemoteRobot, to hold robot information.
+*
+* @author Luke Simmons
+*
+* @date 2022-2-11
+*
+* Declares the struct RemoteRobot to hold information about connected robots.
+*
+* @see src/main.cpp
+*
+* */
 #ifndef CONTROL_REMOTEROBOT_HPP
 #define CONTROL_REMOTEROBOT_HPP
 
@@ -9,10 +17,26 @@
 #include <thread>
 #include <string>
 
+/** @brief Struct to hold information about connected robots.
+ *
+ * Holds information about connected robots and has a static member to hold
+ * list of robots. Contains information including identifier of robot and the
+ * last time the robot communicated with the client, which is used to disconnect
+ * from it after a specified timeout.
+ *
+ * @see src/RemoteRobot.cpp
+ * @see src/main.cpp:broadcastListen
+ * @see src/main.cpp:robotList
+ * @see src/main.cpp:contains
+ * @see src/main.cpp:update
+ */
 struct RemoteRobot {
+	/// List to hold robots in
 	static std::vector<RemoteRobot> robotList;
 
+	/// Identifier for the robot
 	std::string tag;
+	/// Last time the robot communicated with the client
 	time_t lastSeenTime{};
 };
 
--- robotcontrollerclient/src/RemoteRobot.cpp
+++ robotcontrollerclient/src/RemoteRobot.cpp
@@ -1,6 +1,16 @@
-//
-// Created by luke on 2/11/22.
-//
+/** @file
+* @brief Implementation for RemoteRobot.
+*
+* @author Luke Simmons
+*
+* @date 2022-2-11
+*
+* Defines static members of the RemoteRobot struct.
+*
+* @see include/RemoteRobot.hpp
+* @see RemoteRobot
+*
+* */
 
 #include <vector>
 #include "RemoteRobot.hpp"

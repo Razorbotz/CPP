e3007a0f641dd34d5fc98d7cb2d82cd7845a15cd
Andrew Burroughs <andrewburroughs17@gmail.com>
2024-04-22 20:11:28 -0500

    Updated robotcontrollerclient to allow use of UInts

---
+++ robotcontrollerclient2/build/CMakeFiles/control.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/team/SoftwareDevelopment/C++/robotcontrollerclient2/src/BinaryMessage.cpp" "/home/team/SoftwareDevelopment/C++/robotcontrollerclient2/build/CMakeFiles/control.dir/src/BinaryMessage.cpp.o"
+  "/home/team/SoftwareDevelopment/C++/robotcontrollerclient2/src/InfoFrame.cpp" "/home/team/SoftwareDevelopment/C++/robotcontrollerclient2/build/CMakeFiles/control.dir/src/InfoFrame.cpp.o"
+  "/home/team/SoftwareDevelopment/C++/robotcontrollerclient2/src/InfoItem.cpp" "/home/team/SoftwareDevelopment/C++/robotcontrollerclient2/build/CMakeFiles/control.dir/src/InfoItem.cpp.o"
+  "/home/team/SoftwareDevelopment/C++/robotcontrollerclient2/src/control.cpp" "/home/team/SoftwareDevelopment/C++/robotcontrollerclient2/build/CMakeFiles/control.dir/src/control.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/harfbuzz"
+  "/cygwin64/usr/include/SDL2"
+  "/usr/include/SDL2"
+  "/usr/include/glib-2.0"
+  "/usr/lib/glib-2.0/include"
+  "/usr/include/glibmm-2.4"
+  "/usr/include/glibmm-2.4/glibmm"
+  "/usr/lib/glibmm-2.4/include"
+  "/usr/include/sigc++-2.0"
+  "/usr/include/sigc++-2.0/sigc++"
+  "/usr/lib/sigc++-2.0/include"
+  "/usr/include/giomm-2.4"
+  "/usr/lib/giomm-2.4/include"
+  "/usr/include/pango-1.0"
+  "/usr/include/pangomm-1.4"
+  "/usr/lib/pangomm-1.4/include"
+  "/usr/include/cairo"
+  "/usr/include/cairomm-1.0"
+  "/usr/include/freetype2"
+  "/usr/include/gdkmm-3.0"
+  "/usr/lib/gdkmm-3.0/include"
+  "/usr/include/gdk-pixbuf-2.0"
+  "/usr/include/gtk-3.0"
+  "/usr/include/gtkmm-3.0"
+  "/usr/lib/gktmm"
+  "/usr/lib/gtkmm-3.0/include"
+  "/usr/include/atk-1.0"
+  "/usr/include/atkmm-1.6"
+  "/usr/lib/aarch64-linux-gnu/glib-2.0/include"
+  "/usr/lib/aarch64-linux-gnu/glibmm-2.4/include"
+  "/usr/lib/aarch64-linux-gnu/sigc++-2.0/include"
+  "/usr/lib/aarch64-linux-gnu/giomm-2.4/include"
+  "/usr/lib/aarch64-linux-gnu/pangomm-1.4/include"
+  "/usr/lib/aarch64-linux-gnu/gdkmm-3.0/include"
+  "/usr/lib/aarch64-linux-gnu/gtkmm-3.0/include"
+  "/usr/lib/arm-linux-gnueabihf/glib-2.0/include"
+  "/usr/lib/arm-linux-gnueabihf/glibmm-2.4/include"
+  "/usr/lib/arm-linux-gnueabihf/sigc++-2.0/include"
+  "/usr/lib/arm-linux-gnueabihf/giomm-2.4/include"
+  "/usr/lib/arm-linux-gnueabihf/pangomm-1.4/include"
+  "/usr/lib/arm-linux-gnueabihf/gdkmm-3.0/include"
+  "/usr/lib/arm-linux-gnueabihf/gtkmm-3.0/include"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "/usr/lib/x86_64-linux-gnu/glibmm-2.4/include"
+  "/usr/lib/x86_64-linux-gnu/sigc++-2.0/include"
+  "/usr/lib/x86_64-linux-gnu/giomm-2.4/include"
+  "/usr/lib/x86_64-linux-gnu/pangomm-1.4/include"
+  "/usr/lib/x86_64-linux-gnu/gdkmm-3.0/include"
+  "/usr/lib/x86_64-linux-gnu/gtkmm-3.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ robotcontrollerclient2/build/CMakeFiles/control.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/control.dir/src/BinaryMessage.cpp.o"
+  "CMakeFiles/control.dir/src/InfoFrame.cpp.o"
+  "CMakeFiles/control.dir/src/InfoItem.cpp.o"
+  "CMakeFiles/control.dir/src/control.cpp.o"
+  "control"
+  "control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

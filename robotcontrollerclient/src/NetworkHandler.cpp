b7d5488b6a47287ebe922935d9a26cd6b206208e
Andrew Burroughs <43488350+andrewburroughs@users.noreply.github.com>
2026-02-06 12:10:18 -0600

    Added simulated network traffic

--- robotcontrollerclient/src/NetworkHandler.cpp
+++ robotcontrollerclient/src/NetworkHandler.cpp
@@ -524,6 +524,7 @@ static void connectToVideoServer(VideoServerUI& ui, Glib::Dispatcher& dispatcher
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                std::chrono::steady_clock::now() - startTime).count() < 4) {
 =======
                std::chrono::steady_clock::now() - startTime).count() < 2) {
@@ -537,6 +538,9 @@ static void connectToVideoServer(VideoServerUI& ui, Glib::Dispatcher& dispatcher
 =======
                std::chrono::steady_clock::now() - startTime).count() < 2) {
 >>>>>>> 3801b90 (Working on dynamic resize for widgets to support better WSL integration)
+=======
+               std::chrono::steady_clock::now() - startTime).count() < 4) {
+>>>>>>> c790928 (Added simulated network traffic)
 
         socklen_t from_len = video_addr_len;
 
